f765446ad58f42fe010281766e7a1afbef3957a3
rakuishi <rakuishi@gmail.com>
2019-07-07 21:40:57 +0900

    Put a sphere on current location

--- app/src/main/java/com/rakuishi/guidepost/MainActivity.kt
+++ app/src/main/java/com/rakuishi/guidepost/MainActivity.kt
@@ -3,6 +3,7 @@ package com.rakuishi.guidepost
 import android.Manifest
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.location.Location
 import android.os.Bundle
 import android.widget.TextView
@@ -11,16 +12,27 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.Observer
+import com.google.ar.core.Pose
 import com.google.ar.core.TrackingState
+import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.FrameTime
 import com.google.ar.sceneform.Scene
+import com.google.ar.sceneform.math.Vector3
+import com.google.ar.sceneform.rendering.MaterialFactory
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.rendering.ShapeFactory
 import com.google.ar.sceneform.ux.ArFragment
+import kotlin.math.cos
+import kotlin.math.sin
+
 
 class MainActivity : AppCompatActivity(), Scene.OnUpdateListener {
 
     private val REQUEST_PERMISSION: Int = 1000
     private lateinit var arFragment: ArFragment
     private lateinit var textView: TextView
+    private var sphereRenderable: ModelRenderable? = null
+    private var isAlreadyRendered: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +43,11 @@ class MainActivity : AppCompatActivity(), Scene.OnUpdateListener {
         arFragment.arSceneView.scene.addOnUpdateListener(this)
 
         textView = findViewById(R.id.text_view)
+
+        MaterialFactory.makeOpaqueWithColor(this, com.google.ar.sceneform.rendering.Color(Color.CYAN))
+            .thenAccept { material ->
+                sphereRenderable = ShapeFactory.makeSphere(0.025f, Vector3(0.0f, 0.0f, 0.0f), material)
+            }
 
         // Start observing
         if (hasAccessFineLocationPermission()) {
@@ -53,8 +70,34 @@ class MainActivity : AppCompatActivity(), Scene.OnUpdateListener {
         devicePositionLiveData.addSource(orientationLiveData, devicePositionObserver)
         devicePositionLiveData.observe(this, Observer { pair ->
             if (pair == null) return@Observer
+            if (isAlreadyRendered) return@Observer
             textView.text = "${pair.first?.latitude}\n${pair.first?.longitude}\n${pair.second}"
+            renderIfPossible(pair.first?.latitude, pair.first?.longitude, pair.second)
         })
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun renderIfPossible(latitude: Double?, longitude: Double?, orientation: Double?) {
+        if (isAlreadyRendered || latitude == null || longitude == null || orientation == null || sphereRenderable == null) return
+        val camera = arFragment.arSceneView.arFrame!!.camera
+        if (camera.trackingState != TrackingState.TRACKING) return
+
+        val distance = LocationUtils.distance(latitude, longitude, latitude, longitude)
+        val bearing = LocationUtils.bearing(latitude, longitude, latitude, longitude)
+        val rotation = bearing - orientation
+        val radRotation = Math.toRadians(rotation)
+        val zRotated = (-distance * cos(radRotation)).toFloat()
+        val xRotated = (distance * sin(radRotation)).toFloat()
+        val y = camera.displayOrientedPose.ty()
+        val translation = Pose.makeTranslation(xRotated, y, zRotated)
+        val pose = camera.displayOrientedPose.compose(translation).extractTranslation()
+        val anchor = arFragment.arSceneView.session!!.createAnchor(pose)
+        val anchorNode = AnchorNode(anchor)
+        anchorNode.renderable = sphereRenderable
+        anchorNode.setParent(arFragment.arSceneView.scene)
+
+        textView.text = "Latitude: $latitude\nLongitude: $longitude\nOrientation: $orientation\nDistance: $distance"
+        isAlreadyRendered = true
     }
 
     // region AR
