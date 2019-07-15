8959b33afb9c4c49c917bcefb206f9aac66174d5
rakuishi <rakuishi@gmail.com>
2019-07-15 15:51:09 +0900

    Add filter for location

--- app/src/main/java/com/rakuishi/guidepost/LocationLiveData.kt
+++ app/src/main/java/com/rakuishi/guidepost/LocationLiveData.kt
@@ -3,8 +3,10 @@ package com.rakuishi.guidepost
 import android.annotation.SuppressLint
 import android.content.Context
 import android.location.Location
+import android.os.SystemClock
 import androidx.lifecycle.LiveData
 import com.google.android.gms.location.*
+
 
 class LocationLiveData(context: Context) : LiveData<Location>() {
 
@@ -12,8 +14,9 @@ class LocationLiveData(context: Context) : LiveData<Location>() {
         LocationServices.getFusedLocationProviderClient(context)
     private val callback: LocationCallback = object : LocationCallback() {
         override fun onLocationResult(result: LocationResult?) {
-            val location = result?.lastLocation ?: return
-            value = location
+            val location = filteredLocation(result)
+            if (location != null)
+                value = location
         }
     }
 
@@ -31,5 +34,23 @@ class LocationLiveData(context: Context) : LiveData<Location>() {
     override fun onInactive() {
         super.onInactive()
         fusedLocationProviderClient.removeLocationUpdates(callback)
+    }
+
+    // https://link.medium.com/5me7uTDVkY
+    private fun filteredLocation(result: LocationResult?): Location? {
+        val location = result?.lastLocation ?: return null
+
+        // discard the location 10secs ago
+        val age = (SystemClock.elapsedRealtimeNanos() - location.elapsedRealtimeNanos) / 1000000
+        if (age > 10 * 1000) {
+            return null
+        }
+
+        // adopt location within 0~20m
+        if (0 > location.accuracy || location.accuracy > 20) {
+            return null
+        }
+
+        return location
     }
 }
